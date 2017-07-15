07567b3bbe9a9e4198984d3a1d53bc7ae76f7459
Katye <katye333@gmail.com>
2017-07-15 15:59:04 -0400

    Lesson 4, Part 3 - Rendering UI with External Data
    Using an ajax request along with the Contacts API to remove a contact from the database.

--- src/App.js
+++ src/App.js
@@ -3,43 +3,22 @@ import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
-
-    /*
-        Array will be populated using data from the 
-        database that we obtain by using ajax requests.
-    */
     state = {
         contacts: []
     }
 
-    /*
-        When our component mounts, we make an API request
-        and when that request resolves, our then() function
-        will be invoked with our contacts data. Then when 
-        we have those contacts, we call setState() which 
-        updates our state causing a re-render of the component,
-        passing those new contacts down to our list contact
-        component which should finally render them. 
-    */
     componentDidMount() {
         ContactsAPI.getAll().then((contacts) => {
-
-            /*
-                The value is the contacts we're getting
-                from our API request but because the key
-                and value are the same, we can delete one
-                of them from the object entirely
-                (Including the semi-colon)
-
-                Original: this.setState({ contacts: contacts })
-            */
             this.setState({ contacts })
         })
     }
     removeContact = (contact) => {
         this.setState((state) => ({
-            contacts: state.contact.filter((c) => c.id !== contact.id)
+            contacts: state.contacts.filter((c) => c.id !== contact.id)
         }))
+
+        // Removes it from the database 
+        ContactsAPI.remove(contact)
     }
     render() {
         return (
